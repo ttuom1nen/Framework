e7437c5782798ce2bd11e39925d07544d930b96b
ttuom1nen <ttuominen@outlook.com>
2020-03-28 23:20:54 +0200

    Added set method for modifying user properties

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,7 @@ console.log("Hi there!");
 
 const user = new User({ name: "myname", age: 100 });
 
+user.set({ name: "newname", age: 50 });
+
 console.log(user.get("name"));
 console.log(user.get("age"));
