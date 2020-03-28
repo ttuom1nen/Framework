9c9a1d67d70446b37f35788016ea82bff63eeeda
ttuom1nen <ttuominen@outlook.com>
2020-03-28 23:13:55 +0200

    Added user class

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
+import { User } from "./models/User";
+
 console.log("Hi there!");
+
+const user = new User({ name: "myname", age: 100 });
+
+console.log(user.get("name"));
+console.log(user.get("age"));
