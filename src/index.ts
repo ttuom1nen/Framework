1d09b8702ce67e9eed2f159c967ba03aa7d4564b
ttuom1nen <ttuominen@outlook.com>
2020-03-29 23:19:48 +0300

    Separated eventing

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import { User } from "./models/User";
 
-const user = new User({ id: 1 });
+const user = new User({ name: "Baby", age: 0 });
+//user.save();
 
-user.set({ name: "New name", age: 111 });
-user.save();
+user.events.on("change", () => {
+  console.log("change!");
+});
+user.events.trigger("change");
