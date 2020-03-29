3901b5c8307e35f4a0f87f640d63cef3e63430a9
ttuom1nen <ttuominen@outlook.com>
2020-03-29 18:48:56 +0300

    Added data fetching method

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,5 @@
 import { User } from "./models/User";
 
-const user = new User({ name: "myname", age: 100 });
+const user = new User({ id: 1 });
 
-// Test eventing
-user.on("change", () => {
-  console.log("Change #1");
-});
-user.on("newEvent", () => {
-  console.log("newEvent triggered");
-});
-
-user.trigger("newEvent");
+user.fetch();
