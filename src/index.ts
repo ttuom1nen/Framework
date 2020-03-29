c8d2ef45bac8af9c58bacd7c26f6dff732cc77a8
ttuom1nen <ttuominen@outlook.com>
2020-03-29 13:21:37 +0300

    Added event trigger

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import { User } from "./models/User";
 
-console.log("Hi there!");
-
 const user = new User({ name: "myname", age: 100 });
 
 // Test eventing
-user.on("change", () => {});
-user.on("newEvent", () => {});
+user.on("change", () => {
+  console.log("Change #1");
+});
+user.on("newEvent", () => {
+  console.log("newEvent triggered");
+});
 
-console.log(user);
+user.trigger("newEvent");
