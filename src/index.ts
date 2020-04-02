c786c43e3bf77269182820355e2c74596c331776
ttuom1nen <ttuominen@outlook.com>
2020-04-02 23:04:29 +0300

    Implemented passthroughs

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import { User } from "./models/User";
 
 const user = new User({ name: "Baby", age: 0 });
 
-user.events.on("change", () => {
-  console.log("change!");
-});
-user.events.trigger("change");
+// user.on("change", () => {
+//   console.log("User was changed!");
+// });
