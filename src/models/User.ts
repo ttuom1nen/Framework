69dbc33872915d31fa126a3d7da1064fadbc255b
ttuom1nen <ttuominen@outlook.com>
2020-04-03 22:27:07 +0300

    Fetch and save methods

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,3 +1,4 @@
+import { AxiosResponse } from "axios";
 import { Eventing } from "./Eventing";
 import { Sync } from "./Sync";
 import { Attributes } from "./Attributes";
@@ -35,4 +36,27 @@ export class User {
     this.attributes.set(update);
     this.events.trigger("change");
   };
+
+  fetch(): void {
+    const id = this.attributes.get("id");
+
+    if (typeof id !== "number") {
+      throw new Error("Cannot fetch without an id");
+    }
+
+    this.sync.fetch(id).then((response: AxiosResponse): void => {
+      this.set(response.data);
+    });
+  }
+
+  save(): void {
+    this.sync
+      .save(this.attributes.getAll())
+      .then((response: AxiosResponse): void => {
+        this.trigger("save");
+      })
+      .catch(() => {
+        this.trigger("error");
+      });
+  }
 }
