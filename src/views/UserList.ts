bb71aaf61f9b2d31aef269730fa88be4a978f322
ttuom1nen <ttuominen@outlook.com>
2020-04-10 17:19:38 +0300

    Replaced UserShow with UserEdit

--- src/views/UserList.ts
+++ src/views/UserList.ts
@@ -1,9 +1,9 @@
 import { CollectionView } from "./CollectionView";
 import { User, UserProps } from "../models/User";
-import { UserShow } from "./UserShow";
+import { UserEdit } from "./UserEdit";
 
 export class UserList extends CollectionView<User, UserProps> {
   renderItem(model: User, itemParent: Element): void {
-    new UserShow(itemParent, model).render();
+    new UserEdit(itemParent, model).render();
   }
 }
