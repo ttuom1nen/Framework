e7e27d0e1a902cfeea741ae85ddb97ba105510ef
ttuom1nen <ttuominen@outlook.com>
2020-04-10 16:54:48 +0300

    Modified testing logic

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,19 @@
-import { UserEdit } from "./views/UserEdit";
-import { User } from "./models/User";
+import { UserList } from "./views/UserList";
+import { Collection } from "./models/Collection";
+import { UserProps, User } from "./models/User";
 
-const user = User.buildUser({ name: "Jessica", age: 20 });
+const users = new Collection(
+  "http://localhost:3000/Users",
+  (json: UserProps) => {
+    return User.buildUser(json);
+  }
+);
 
-const root = document.getElementById("root");
+users.on("change", () => {
+  const root = document.getElementById("root");
 
-if (root) {
-  const userEdit = new UserEdit(root, user);
-  userEdit.render();
-} else {
-  throw new Error("Root element not found!");
-}
+  if (root) {
+    new UserList(root, users).render();
+  }
+});
+users.fetch();
